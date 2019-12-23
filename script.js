29c78e26befa2cd9f40c614676c607807abda0f3
edward <ed_010@mail.ru>
2019-12-23 20:44:53 +0300

    up

---
+++ script.js
@@ -0,0 +1,13 @@
+var ansi = require('ansi');
+cursor = ansi(process.stdout);
+ 
+
+cursor
+  .red()                 
+  .bg.white()             
+  .write('Hello World!') 
+  .bg.reset()                 
+  .write('\n')           
+
+  cursor.hex('#660000').underline()
+  // со звуком не получилось(
